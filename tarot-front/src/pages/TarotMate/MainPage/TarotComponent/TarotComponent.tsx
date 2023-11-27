486ffb0f2fe8d5bc0df9e1fae3c5324254b1f947
kabalport <kabalport@gmail.com>
2023-11-27 21:18:44 +0900

    main페이지 디자인꾸미기2

--- tarot-front/src/pages/TarotMate/MainPage/TarotComponent/TarotComponent.tsx
+++ tarot-front/src/pages/TarotMate/MainPage/TarotComponent/TarotComponent.tsx
@@ -23,29 +23,30 @@ function TarotComponent() {
 
     // 타로 카드와 Unsplash 이미지 URL 매핑
     const tarotCards = [
-        { name: "광대", image: "../images/major_arcana_fool.png" },
-        { name: "마법사", image: "../images/major_arcana_magician.png" },
-        { name: "여사제", image: "../images/major_arcana_priestess.png" },
-        { name: "여황제", image: "../images/major_arcana_empress.png" },
-        { name: "황제", image: "../images/major_arcana_emperor.png" },
-        { name: "교황", image: "../images/major_arcana_hierophant.png" },
-        { name: "연인들", image: "../images/major_arcana_lovers.png" },
-        { name: "전차", image: "../images/major_arcana_chariot.png" },
-        { name: "힘", image: "../images/major_arcana_strength.png" },
-        { name: "은둔자", image: "../images/major_arcana_hermit.png" },
-        { name: "운명의 수레바퀴", image: "../images/major_arcana_fortune.png" },
-        { name: "정의", image: "../images/major_arcana_justice.png" },
-        { name: "매달린 사람", image: "../images/major_arcana_hanged.png" },
-        { name: "죽음", image: "../images/major_arcana_death.png" },
-        { name: "절제", image: "../images/major_arcana_temperance.png" },
-        { name: "악마", image: "../images/major_arcana_devil.png" },
-        { name: "탑", image: "../images/major_arcana_tower.png" },
-        { name: "별", image: "../images/major_arcana_star.png" },
-        { name: "달", image: "../images/major_arcana_moon.png" },
-        { name: "태양", image: "../images/major_arcana_sun.png" },
-        { name: "심판", image: "../images/major_arcana_judgement.png" },
-        { name: "세계", image: "../images/major_arcana_world.png" }
+        { number: 0, name: "광대", image: "../images/major_arcana_fool.png" },
+        { number: 1, name: "마법사", image: "../images/major_arcana_magician.png" },
+        { number: 2, name: "여사제", image: "../images/major_arcana_priestess.png" },
+        { number: 3, name: "여황제", image: "../images/major_arcana_empress.png" },
+        { number: 4, name: "황제", image: "../images/major_arcana_emperor.png" },
+        { number: 5, name: "교황", image: "../images/major_arcana_hierophant.png" },
+        { number: 6, name: "연인들", image: "../images/major_arcana_lovers.png" },
+        { number: 7, name: "전차", image: "../images/major_arcana_chariot.png" },
+        { number: 8, name: "힘", image: "../images/major_arcana_strength.png" },
+        { number: 9, name: "은둔자", image: "../images/major_arcana_hermit.png" },
+        { number: 10, name: "운명의 수레바퀴", image: "../images/major_arcana_fortune.png" },
+        { number: 11, name: "정의", image: "../images/major_arcana_justice.png" },
+        { number: 12, name: "매달린 사람", image: "../images/major_arcana_hanged.png" },
+        { number: 13, name: "죽음", image: "../images/major_arcana_death.png" },
+        { number: 14, name: "절제", image: "../images/major_arcana_temperance.png" },
+        { number: 15, name: "악마", image: "../images/major_arcana_devil.png" },
+        { number: 16, name: "탑", image: "../images/major_arcana_tower.png" },
+        { number: 17, name: "별", image: "../images/major_arcana_star.png" },
+        { number: 18, name: "달", image: "../images/major_arcana_moon.png" },
+        { number: 19, name: "태양", image: "../images/major_arcana_sun.png" },
+        { number: 20, name: "심판", image: "../images/major_arcana_judgement.png" },
+        { number: 21, name: "세계", image: "../images/major_arcana_world.png" }
     ];
+
 
 // Replace "../images/" with the actual path to the directory where the images are stored.
 
