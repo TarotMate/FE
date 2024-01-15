092d00f2d314920a54fea50b45c2ae61474cf6c2
kabalport <kabalport@gmail.com>
2024-01-15 11:53:56 +0900

    리팩토링q1

--- tarot-front/src/pages/detailPage/components/DisplayTextForSelectedTab.tsx
+++ tarot-front/src/pages/detailPage/components/DisplayTextForSelectedTab.tsx
@@ -3,14 +3,12 @@ import { Typography } from "@mui/material";
 
 interface DisplayTextForSelectedTabProps {
     title: string;
-    subtitle: string;
 }
 
-const DisplayTextForSelectedTab: React.FC<DisplayTextForSelectedTabProps> = ({ title, subtitle }) => {
+const DisplayTextForSelectedTab: React.FC<DisplayTextForSelectedTabProps> = ({ title }) => {
     return (
         <>
             <Typography variant="h5" style={{ fontWeight: 'bold' }}>{title}</Typography>
-            <Typography variant="subtitle2">{subtitle}</Typography>
         </>
     );
 };
